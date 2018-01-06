4e33789c0e75a35fbbffbeeaa8ece61f6c90ee2f
Ali Aburas <aburasa@nondalton.eecs.oregonstate.edu>
2018-01-05 20:30:25 -0800

    add Latex folder

---
+++ Latex/README.md
@@ -0,0 +1,1 @@
+The Latex directory contains 2 different Latex examples
---
+++ Latex/example-2/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+	printf("Hello CS361. You passed %d arguments\n", argc);
+	return 0;
+}
